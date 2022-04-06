ddd1b0557e8e340b921229dfdfe64f4c14bc2cdf
RyanChen_YLC <r08522622@ntu.edu.tw>
2022-04-06 14:36:38 +0800

    add modbus-related files

---
+++ src/rrbot_hw/include/rrbot_hw/motor_driver.h
@@ -0,0 +1,62 @@
+#include "modbus.h"
+
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
+
+
+namespace Motor
+{
+    class MotorDriver : public SerialModbus
+    {
+    private:
+        std::shared_ptr<boost::mutex> p_func_mutex;
+
+        const uint8_t Broadcast         = 0x00;
+        const uint8_t MOTOR_ID          = 0x01;
+        const uint8_t FC_MasterSendCMD  = 0x65;
+        const uint8_t CMD_ISTOP_Echo    = 0x00;
+        const uint8_t CMD_ISTOP_No_Echo = 0x64;
+        const uint8_t CMD_JG_Echo       = 0x0A;
+        const uint8_t CMD_JG_No_Echo    = 0x6E;
+        const uint8_t CMD_FREE_Echo     = 0x05;
+        const uint8_t CMD_FREE_No_Echo  = 0x69;
+        const uint8_t CMD_SVON_Echo     = 0x06;
+        const uint8_t CMD_SVON_No_Echo  = 0x6A;
+        const uint8_t CMD_IMR_Echo      = 0x0B;
+        const uint8_t CMD_IMR_No_Echo   = 0x6F;
+        const uint8_t CMD_CS_Echo       = 0x0E;
+        const uint8_t CMD_CS_No_Echo    = 0x72;
+        const uint8_t CMD_CMR_Echo      = 0x0F;
+        const uint8_t CMD_CMR_No_Echo   = 0x73;
+        const uint8_t CMD_CMA_Echo      = 0x10;
+        const uint8_t CMD_CMA_No_Echo   = 0x74;
+        const uint8_t CMD_NULL_Echo     = 0x63;
+        const uint8_t CMD_NULL_No_Echo  = 0x77;
+        
+        
+
+        const int rcv_size              =    8;
+
+
+    public:
+        MotorDriver(const std::string _serial_port, const int _baud_rate);
+        virtual ~MotorDriver();
+
+        void JG(uint16_t _cmd_rpm, bool is_echo);
+        void ISTOP(bool is_echo);
+        void FREE(bool is_echo);
+        void SVON(bool is_echo);
+        void IMR(uint16_t _index, uint16_t _step, bool is_echo);
+        void CS(uint16_t _index, uint16_t _step, bool is_echo);
+        void CMR(uint16_t _index, uint16_t _step, bool is_echo);
+        void CMA(uint16_t _index, uint16_t _step, bool is_echo);
+        void NULL_TO_ECHO(bool is_echo);
+
+    
+
+
+
+    };
+} // namespace Motor
+
+#endif
---
+++ src/rrbot_hw/src/motor_driver.cpp
@@ -0,0 +1,390 @@
+#include "include/motor_driver.h"
+
+namespace Motor{
+
+    union unionType
+    {
+        uint8_t _data_byte[2];
+        uint16_t _data;
+    };
+
+
+    MotorDriver::MotorDriver(const std::string _serial_port = "/dev/ttyUSB0", const int _baud_rate = 115200) : SerialModbus(_serial_port, _baud_rate){
+        p_func_mutex = std::shared_ptr<boost::mutex>{new boost::mutex};
+    }
+    MotorDriver::~MotorDriver(){
+        // STOP Motor
+        this->ISTOP(false);
+        // this->FREE(false);
+    }
+
+
+
+    void MotorDriver::JG(uint16_t _cmd_rpm, bool is_echo = false){
+
+        /* ==============================================================================
+            *     0 < _cmd_rpm < 4000  :  CW
+            * -4000 < _cmd_rpm <    0  : CCW
+            * 
+            * Example: Set +300 rpm / No Echo
+            * 0 65 1 1 6e 0 0 1 2c 34 72
+            * 
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_JG_Echo : this->CMD_JG_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = 0x00;
+        _data_2._data = _cmd_rpm;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send JG Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void MotorDriver::ISTOP(bool is_echo = false){
+
+        /* ==============================================================================
+            * Example: No Echo
+            * 0 65 1 1 64 0 0 0 0 ac 3e
+            * 
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+        
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_ISTOP_Echo : this->CMD_ISTOP_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = 0x00;
+        _data_2._data = 0x00;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send ISTOP Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void MotorDriver::FREE(bool is_echo = false){
+
+        /* ==============================================================================
+            * Example: No Echo
+            * 0 65 1 1 69 0 0 0 0 81 ff
+            * 
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+        
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_FREE_Echo : this->CMD_FREE_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = 0x00;
+        _data_2._data = 0x00;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send FREE Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void MotorDriver::SVON(bool is_echo = false){
+
+        /* ==============================================================================
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_SVON_Echo : this->CMD_SVON_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = 0x00;
+        _data_2._data = 0x00;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send SVON Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+    void MotorDriver::IMR(uint16_t _index, uint16_t _step, bool is_echo = false){
+        
+        /* ==============================================================================
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_IMR_Echo : this->CMD_IMR_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = _index;
+        _data_2._data = _step;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send IMR Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+    void MotorDriver::CS(uint16_t _index, uint16_t _step, bool is_echo = false){
+        
+        /* ==============================================================================
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_CS_Echo : this->CMD_CS_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = _index;
+        _data_2._data = _step;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send CS Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+    void MotorDriver::CMR(uint16_t _index, uint16_t _step, bool is_echo = false){
+        
+        /* ==============================================================================
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_CMR_Echo : this->CMD_CMR_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = _index;
+        _data_2._data = _step;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send CMR Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+    void MotorDriver::CMA(uint16_t _index, uint16_t _step, bool is_echo = false){
+        
+        /* ==============================================================================
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_CMA_Echo : this->CMD_CMA_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = _index;
+        _data_2._data = _step;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send CMA Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+    }
+
+    void MotorDriver::NULL_TO_ECHO(bool is_echo = true){
+        
+        /* ==============================================================================
+            * If echo, receive 8 bytes data per message.
+        ============================================================================== */
+
+        std::vector<uint8_t> p_data;
+        uint8_t _num = 0x01;
+        uint8_t _cmd = is_echo ? this->CMD_NULL_Echo : this->CMD_NULL_No_Echo;
+        unionType _data_1, _data_2;
+        _data_1._data = 0x00;
+        _data_2._data = 0x00;
+
+        p_data.clear();
+        p_data.push_back(this->Broadcast);
+        p_data.push_back(this->FC_MasterSendCMD);
+        p_data.push_back(_num);
+        p_data.push_back(this->MOTOR_ID);
+        p_data.push_back(_cmd);
+        p_data.push_back(_data_1._data_byte[1]);
+        p_data.push_back(_data_1._data_byte[0]);
+        p_data.push_back(_data_2._data_byte[1]);
+        p_data.push_back(_data_2._data_byte[0]);
+        uint16_t crc = this->calculate_CRC(p_data);
+        p_data.push_back(crc);
+        p_data.push_back(crc >> 8);
+        std::vector<char> p_char(p_data.begin(), p_data.end());
+        this->write(p_char);
+
+        std::cout <<  "Send NULL Command: ";
+        for(auto i=0;i<p_char.size();i++){            
+            std::cout << std::hex << (int)p_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+        if(is_echo){
+
+            std::vector<char> rcv_char;
+            
+            {
+                usleep(RESPONSE_DELAY_US);
+                try
+                {
+                    rcv_char = asyncRead(this->rcv_size);
+                }
+                catch(const std::exception& e)
+                {
+                    std::cerr << e.what() << '\n';
+                }
+                std::cout <<  "Received Data: ";
+                for(auto i=0;i<rcv_char.size();i++){            
+                    uint8_t a = rcv_char[i];
+                    std::cout << std::hex << +a << " ";
+                }
+                std::cout << std::endl;
+                
+            }
+
+        }
+    }
+
+
+}
